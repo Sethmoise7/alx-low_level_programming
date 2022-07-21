ca6d7fe28dd671ca31553c59e6deac059342a0e9
Sethmoise7 <uwayosethmoise@gmail.com>
2022-07-21 04:42:12 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,51 +1,65 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - size
- * @s: pointer to string params
- * Return: recursion
- */
+* substring_match - check if a substring after wildcard matches s1
+* @s1: one string
+* @s2: one string
+* @after_wldcd: placeholder for position right after wildcard
+* Return: 1 if matched, 0 if not
+*/
 
-int _strlen_recursion(char *s)
+int substring_match(char *s1, char *s2, char *after_wldcd)
 {
-	if (!*s)
-	{
-		return (0);
-	}
-	return (1 + _strlen_recursion(++s));
-}
 
-/**
- * p1 - palindrome
- * @s: pointer to string
- * @l: position
- * Return: boolena
- */
+	if (*s1 == '\0' && *s2 == '\0')
 
-int p1(char *s, int l)
-{
-	if (l < 1)
-	{
 		return (1);
-	}
 
-	if (*s == *(s + l))
-	{
-		return (p1(s + 1, l - 2));
-	}
-	return (0);
+	if (*s1 == '\0' && *s2 == '*')
+
+		return (substring_match(s1, s2 + 1, s2 + 1));
+
+	if (*s1 == '\0' && *s2 != '\0')
+
+		return (0);
+
+	if (*s2 == '*')
+
+		return (substring_match(s1, s2 + 1, s2 + 1));
+
+	if (*s1 == *s2)
+
+		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+
+	else
+
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+
 }
 
 /**
- * is_palindrome - palindrome
- * @s: pointer to string
- * Return: recursion
- */
+* wildcmp - compare if string with wildcard mattches
+* @s1: one string
+* @s2: one string
+* Return: 1 if matched, 0 if not
+*/
 
-int is_palindrome(char *s)
+int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0' && *s2 == '\0')
 
-	int len = _strlen_recursion(s);
+		return (1);
 
-	return (p1(s, len - 1));
+	if (*s1 == *s2)
+
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	else if (*s2 == '*')
+
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
+
+	else
+
+		return (0);
+
 }
